6608165590c419f4a601077085d3409b0cb38322
prediksi-del <kematianituada493@gmail.com>
2026-05-23 02:34:59 +0700

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
     firebaseUid: { type: String, required: true, unique: true, index: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String }, // Tambahkan kolom ini untuk mencocokkan kredensial login admin
     displayName: { type: String },
     balance: { type: Number, default: 0, min: 0 },
     role: { type: String, enum: ['user', 'admin'], default: 'user' },
