12ab15f98af6106344f680c081d6da53fde93e85
prediksi-del <kematianituada493@gmail.com>
2026-05-23 01:49:40 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 let cachedDb = null;
 
@@ -21,4 +21,4 @@ const connectDB = async () => {
     }
 };
 
-module.exports = connectDB;
+export default connectDB;
