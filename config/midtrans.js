f1ada96f6bce0d45b9285b35c28a7d299a72364a
prediksi-del <kematianituada493@gmail.com>
2026-05-23 00:42:40 +0700

    Refactor midtrans.js to use CoreApi and CommonJS

--- config/midtrans.js
+++ config/midtrans.js
@@ -1,7 +1,9 @@
-import midtransClient from 'midtrans-client';
+const midtransClient = require('midtrans-client');
 
-export const snap = new midtransClient.Snap({
-  isProduction: process.env.MIDTRANS_IS_PRODUCTION === 'true',
-  serverKey: process.env.MIDTRANS_SERVER_KEY,
-  clientKey: process.env.MIDTRANS_CLIENT_KEY
+const coreApi = new midtransClient.CoreApi({
+    isProduction: process.env.MIDTRANS_IS_PRODUCTION === 'true',
+    serverKey: process.env.MIDTRANS_SERVER_KEY,
+    clientKey: process.env.MIDTRANS_CLIENT_KEY
 });
+
+module.exports = coreApi;
